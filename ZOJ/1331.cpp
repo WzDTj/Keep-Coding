f821f8171df5aec451955dc50826ffdfea026a56
Dantong Jin <j.dantong@gmail.com>
2017-03-20 20:39:59 +0800

    modified ZOJ/1331

--- ZOJ/1331.cpp
+++ ZOJ/1331.cpp
@@ -9,13 +9,15 @@ int main() {
     for (int i = 0; i < LENGTH; ++i) list[i] = i * i * i;
 
     for (int i = 6; i < LENGTH; ++i) {
+        
         for (int j = 2; j < i; ++j) {
+
             if (list[i] < list[j]) break;
             for (int k = j+1; k < i; ++k) {
                 if (list[i] < list[j] + list[k]) break;
                 for (int l = k+1; k < i; ++l) {
                     if (list[i] < list[j] + list[k] + list[l]) break;
-                    if (list[i] == list[j] + list[k] + list[l]) cout << "Cubo = " << i << ", Triple = (" << j << "," << k << "," << l << ")" << endl;
+                    if (list[i] == list[j] + list[k] + list[l]) cout << "Cube = " << i << ", Triple = (" << j << "," << k << "," << l << ")" << endl;
                 }
             }
         }
